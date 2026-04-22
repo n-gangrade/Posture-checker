f4828f4329a4a2dff02ea2331df53a598abe4018
Micah-Diffee <mchdiffee@yahoo.com>
2026-04-22 12:10:59 -0500

    Added system notifications

--- electron/preload.js
+++ electron/preload.js
@@ -1,10 +1,8 @@
-const { contextBridge, ipcRenderer } = require("electron");
+const { contextBridge, ipcRenderer } = require('electron');
 
-contextBridge.exposeInMainWorld("electronAPI", {
-  sendNotification: (data) => ipcRenderer.send("send-notification", data),
-  onNotificationData: (callback) =>
-    ipcRenderer.on("notification-data", (event, data) => callback(data)),
-  focusMainWindow: () => ipcRenderer.send("focus-main-window"), // 👈 add this
-  exportSessionData: () => ipcRenderer.invoke("export-session-data"),
-  getSessionStats: () => ipcRenderer.invoke("get-session-stats"),
+contextBridge.exposeInMainWorld('electronAPI', {
+  sendNotification: (data) => ipcRenderer.send('send-notification', data),
+  sendSystemNotification: (data) => ipcRenderer.send('send-system-notification', data), // 👈 add
+  onNotificationData: (callback) => ipcRenderer.on('notification-data', (event, data) => callback(data)),
+  focusMainWindow: () => ipcRenderer.send('focus-main-window'),
 });
